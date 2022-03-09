67e80d0663071c450a8bac2c381df7904741a5f6
3DGAR-XD <ejlc2001@gmail.com>
2022-03-08 19:48:36 -0500

    boostrap update, miss colors

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* eslint-disable prefer-destructuring */
-document.querySelector("[value=Jugar]").disabled = true;
 let puntos = 0;
 
 const api = "https://restcountries.com/v3.1/all";
@@ -7,11 +6,9 @@ fetch(api)
   .then((response) => response.json())
   .then((data) => {
     const country = data[Math.floor(Math.random() * data.length)]; // get a random country
-    document.querySelector("[value=Generar]").addEventListener("click", async () => {
-      document.querySelector("[value=Generar]").toggleAttribute("disabled");
-      document.querySelector("[value=Jugar]").removeAttribute("disabled");
-      document.querySelector("[value=Jugar]").style.backgroundColor = "#090";
-      document.querySelector("[value=Generar]").style.backgroundColor = "#ffff";
+    document.querySelector("button.generar").addEventListener("click", () => {
+      document.querySelector("button.generar").classList.add("disabled");
+      document.querySelector("button.jugar").classList.remove("disabled");
       document.querySelector("strong").innerHTML += country.translations.spa.common;
       document.querySelectorAll("label").forEach((label) => {
         const labelText = label;
@@ -58,49 +55,51 @@ fetch(api)
         document.querySelectorAll(".independent")[1].innerHTML = "Sí";
       }
       // Validación de respuestas
-      document.querySelector("#capital [value=Verificar]").addEventListener("click", () => {
+      document.querySelector("#capital button").addEventListener("click", () => {
         if (document.querySelector("#capital select").value == country.capital) {
           puntos += 1;
           document.querySelector("#capital select").style.backgroundColor = "#00d912";
         } else { document.querySelector("#capital select").style.backgroundColor = "#e30000"; }
-        document.querySelector("#capital [value=Verificar]").disabled = true;
+        document.querySelector("#capital button").disabled = true;
       });
-      document.querySelector("#timezones [value=Verificar]").addEventListener("click", () => {
+      document.querySelector("#timezones button").addEventListener("click", () => {
         if (document.querySelector("#timezones select").value === country.timezones[0]) {
           puntos += 1;
           document.querySelector("#timezones select").style.backgroundColor = "#00d912";
         } else { document.querySelector("#timezones select").style.backgroundColor = "#e30000"; }
-        document.querySelector("#timezones [value=Verificar]").disabled = true;
+        document.querySelector("#timezones button").disabled = true;
       });
-      document.querySelector("#tld [value=Verificar]").addEventListener("click", () => {
+      document.querySelector("#tld button").addEventListener("click", () => {
         if (document.querySelector("#tld select").value === country.tld[0]) {
           puntos += 1;
           document.querySelector("#tld select").style.backgroundColor = "#00d912";
         } else { document.querySelector("#tld select").style.backgroundColor = "#e30000"; }
-        document.querySelector("#tld [value=Verificar]").disabled = true;
+        document.querySelector("#tld button").disabled = true;
       });
-      document.querySelector("#population [value=Verificar]").addEventListener("click", () => {
+      document.querySelector("#population button").addEventListener("click", () => {
         if (document.querySelector("#population select").value === `${country.population} personas`) {
           puntos += 1;
           document.querySelector("#population select").style.backgroundColor = "#00d912";
         } else { document.querySelector("#population select").style.backgroundColor = "#e30000"; }
-        document.querySelector("#population [value=Verificar]").disabled = true;
+        document.querySelector("#population button").disabled = true;
       });
-      document.querySelector("#independent [value=Verificar]").addEventListener("click", () => {
+      document.querySelector("#independent button").addEventListener("click", () => {
         if (document.querySelector("#independent select").value === "Sí" && country.independent === true) {
           puntos += 1;
           document.querySelector("#independent select").style.backgroundColor = "#00d912";
         } else { document.querySelector("#independent select").style.backgroundColor = "#e30000"; }
-        document.querySelector("#independent [value=Verificar]").disabled = true;
+        document.querySelector("#independent button").disabled = true;
       });
     });
-    document.querySelector("[value=Jugar]").addEventListener("click", () => {
-      document.querySelector(".home").style.display = "none";
-      document.querySelector(".game").style.display = "block";
+    document.querySelector("button.jugar").addEventListener("click", () => {
+      document.querySelector(".home").classList.add("d-none");
+      document.querySelector(".game").classList.remove("d-none");
     }, !1);
     document.querySelector("[type=submit]").addEventListener("click", () => {
-      document.querySelector(".game").style.display = "none";
-      document.querySelector(".result").style.display = "block";
-      if (puntos === 1) { document.getElementById("score").innerHTML = `${puntos} punto`; } else { document.getElementById("score").innerHTML = `${puntos} puntos`; }
+      document.querySelector(".game").classList.add("d-none");
+      document.querySelector(".result").classList.remove("d-none");
+      document.getElementById("score").style.fontWeight = "bold";
+      if (puntos === 1) document.getElementById("score").innerHTML = `${puntos} punto`;
+      else document.getElementById("score").innerHTML = `${puntos} puntos`;
     }, !1);
   });
